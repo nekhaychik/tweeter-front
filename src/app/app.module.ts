32dfa7e8e5d45cd129d991c3bd919e7086846e5e
nekhaychik <irina.nekhaychik.i@gmail.com>
2023-01-26 16:42:37 +0100

    add: dependencies

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,52 @@
+import { NgDompurifySanitizer } from "@tinkoff/ng-dompurify";
+import { TuiRootModule, TuiDialogModule, TuiAlertModule, TUI_SANITIZER } from "@taiga-ui/core";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { TuiAvatarModule } from '@taiga-ui/kit';
+import { TuiTabsModule } from '@taiga-ui/kit';
+import { TuiHostedDropdownModule } from '@taiga-ui/core';
+import { TuiDataListModule } from '@taiga-ui/core';
+import { TuiSvgModule } from '@taiga-ui/core';
+import { TuiButtonModule } from '@taiga-ui/core';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { LogoComponent } from './logo/logo.component';
+import { AvatarComponent } from './avatar/avatar.component';
+import { SvgImageComponent } from './svg-image/svg-image.component';
+import { MainComponent } from './main/main.component';
+import { UserInfoComponent } from './user-info/user-info.component';
+import { ButtonComponent } from './button/button.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LogoComponent,
+    AvatarComponent,
+    SvgImageComponent,
+    MainComponent,
+    UserInfoComponent,
+    ButtonComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    TuiRootModule,
+    TuiDialogModule,
+    TuiAlertModule,
+    TuiAvatarModule,
+    TuiTabsModule,
+    TuiHostedDropdownModule,
+    TuiDataListModule,
+    TuiSvgModule,
+    TuiButtonModule,
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [{ provide: TUI_SANITIZER, useClass: NgDompurifySanitizer }],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
