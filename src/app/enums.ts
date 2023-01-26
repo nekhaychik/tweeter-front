fa3cf2b649cbd544ba35f59975e946e12547d650
nekhaychik <irina.nekhaychik.i@gmail.com>
2023-01-26 16:45:24 +0100

    add: svg-image component and images

---
+++ src/app/enums.ts
@@ -0,0 +1,16 @@
+export enum Size {
+  xs = 'xs',
+  s = 's',
+  m = 'm',
+  xxl = 'xxl',
+}
+
+export enum ButtonSize {
+  s = 's',
+  m = 'm',
+}
+
+export enum ButtonAppearance {
+ primary = 'primary',
+
+}
---
+++ src/app/svg-image/svg-image.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-svg-image',
+  templateUrl: './svg-image.component.html',
+  styleUrls: ['./svg-image.component.css']
+})
+export class SvgImageComponent {
+imagePath: any;
+
+}
