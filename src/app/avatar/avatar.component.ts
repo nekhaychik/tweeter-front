c935bbfdd17553f10da466dc83bda1d04d46e95b
nekhaychik <irina.nekhaychik.i@gmail.com>
2023-01-26 16:44:21 +0100

    add: avatar component

---
+++ src/app/avatar/avatar.component.ts
@@ -0,0 +1,15 @@
+import { Component, Input } from '@angular/core';
+import { Size } from '../enums';
+
+@Component({
+  selector: 'app-avatar',
+  templateUrl: './avatar.component.html',
+  styleUrls: ['./avatar.component.css'],
+})
+export class AvatarComponent {
+  @Input()
+  public name: string = 'User Name';
+
+  @Input()
+  public size: Size = Size.s;
+}
