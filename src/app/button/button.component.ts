21c8fbc8b0c1626209834f903e8ee8780b103246
nekhaychik <irina.nekhaychik.i@gmail.com>
2023-01-26 16:44:46 +0100

    add: button component

---
+++ src/app/button/button.component.ts
@@ -0,0 +1,18 @@
+import { Component, Input } from '@angular/core';
+import { ButtonAppearance, ButtonSize } from '../enums';
+
+@Component({
+  selector: 'app-button',
+  templateUrl: './button.component.html',
+  styleUrls: ['./button.component.css'],
+})
+export class ButtonComponent {
+  @Input()
+  public appearance: ButtonAppearance = ButtonAppearance.primary;
+
+  @Input()
+  public size: ButtonSize = ButtonSize.m;
+
+  @Input()
+  public text: string = 'Button';
+}
