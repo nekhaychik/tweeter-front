70939584aba2023bbb63c4162f776bbeb951e132
nekhaychik <irina.nekhaychik.i@gmail.com>
2023-02-01 15:41:43 +0100

    add: auth guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,29 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+// Guard
+import { AuthGuard } from './guards/auth.guard';
+
+// Components
+import { MainComponent } from './main/main.component';
+
 const routes: Routes = [
+  {
+    path: 'private',
+    canActivate: [AuthGuard],
+    component: MainComponent,
+    // loadChildren: () =>
+    //   import('./private/private.module').then((m) => m.PrivateModule),
+  },
   {
     path: 'public',
     loadChildren: () =>
       import('./public/public.module').then((m) => m.PublicModule),
+  },
+  {
+    path: '**',
+    redirectTo: 'public',
+    pathMatch: 'full',
   },
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,11 @@
-import { NgDompurifySanitizer } from "@tinkoff/ng-dompurify";
-import { TuiRootModule, TuiDialogModule, TuiAlertModule, TUI_SANITIZER } from "@taiga-ui/core";
-import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { NgDompurifySanitizer } from '@tinkoff/ng-dompurify';
+import {
+  TuiRootModule,
+  TuiDialogModule,
+  TuiAlertModule,
+  TUI_SANITIZER,
+} from '@taiga-ui/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
@@ -27,9 +32,10 @@ import { TweetComponent } from './tweet/tweet.component';
 
 import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { JwtModule } from '@auth0/angular-jwt';
+import { ACCESS_TOKEN_FIELD } from './public/constants/constants';
 
 export function tokenGetter() {
-  return localStorage.getItem('nestjs_chat_app');
+  return localStorage.getItem(ACCESS_TOKEN_FIELD);
 }
 
 @NgModule({
@@ -68,9 +74,7 @@ export function tokenGetter() {
       },
     }),
   ],
-  providers: [
-    { provide: TUI_SANITIZER, useClass: NgDompurifySanitizer },
-  ],
+  providers: [{ provide: TUI_SANITIZER, useClass: NgDompurifySanitizer }],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
