f24cc6ec1b3270a5a74693e781b7ae4e71c66bdf
nekhaychik <irina.nekhaychik.i@gmail.com>
2023-02-01 15:24:04 +0100

    add: tweet and tweet-news components

---
+++ src/app/tweet/tweet.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { ButtonAppearance, ButtonSize, Size } from '../enums';
+
+@Component({
+  selector: 'app-tweet',
+  templateUrl: './tweet.component.html',
+  styleUrls: ['./tweet.component.css'],
+})
+export class TweetComponent implements OnInit {
+  avatarSize = Size.m;
+  buttonAppearance = ButtonAppearance.mono;
+  buttonSize = ButtonSize.m;
+  tweetButtons = ['Comments', 'Retweets', 'Likes', 'Saved'];
+
+  constructor() {}
+
+  ngOnInit() {}
+}
---
+++ src/app/tweets-news/tweets-news.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tweets-news',
+  templateUrl: './tweets-news.component.html',
+  styleUrls: ['./tweets-news.component.css']
+})
+export class TweetsNewsComponent {
+  tweets = [1, 2, 3, 4]
+}
