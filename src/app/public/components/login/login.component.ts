458386f249f4e3f2055acbe21205bbdb1ce14ede
nekhaychik <irina.nekhaychik.i@gmail.com>
2023-02-01 15:27:36 +0100

    add: login component

---
+++ src/app/public/components/login/login.component.ts
@@ -0,0 +1,69 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { tap } from 'rxjs/operators';
+
+// Services
+import { AuthService } from '../../services/auth-service/auth.service';
+
+// Interface
+import { LoginControls } from 'src/app/model/control.interface';
+
+// Constants
+import { PASSWORD_MIN_LENGTH } from '../../constants/constants';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css'],
+})
+export class LoginComponent implements OnInit {
+  public loginForm: FormGroup = new FormGroup({});
+  public formControls: typeof LoginControls = LoginControls;
+
+  public ngOnInit(): void {
+    this.loginForm.addControl(
+      LoginControls.email,
+      new FormControl(
+        null,
+        Validators.compose([Validators.required, Validators.email])
+      )
+    );
+
+    this.loginForm.addControl(
+      LoginControls.password,
+      new FormControl(
+        null,
+        Validators.compose([
+          Validators.required,
+          Validators.minLength(PASSWORD_MIN_LENGTH),
+        ])
+      )
+    );
+  }
+
+  public constructor(
+    private authService: AuthService,
+    private router: Router
+  ) {}
+
+  public login(): void {
+    if (this.loginForm.valid) {
+      this.authService
+        .login({
+          email: this.email.value,
+          password: this.password.value,
+        })
+        .pipe(tap(() => this.router.navigate(['./public/sign-up'])))
+        .subscribe();
+    }
+  }
+
+  public get email(): FormControl {
+    return this.loginForm.get(LoginControls.email) as FormControl;
+  }
+
+  public get password(): FormControl {
+    return this.loginForm.get(LoginControls.password) as FormControl;
+  }
+}
---
+++ src/app/public/constants/constants.ts
@@ -0,0 +1,6 @@
+export const USERNAME_MIN_LEGTH: number = 2;
+export const USERNAME_MAX_LENGTH: number = 100;
+export const PASSWORD_MIN_LENGTH: number = 6;
+export const API: string = 'http://localhost:3000';
+export const ACCESS_TOKEN_FIELD: string = 'user_access_token';
+export const USER_EMAIL_FIELD: string = 'user_email';
